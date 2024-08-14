ae131c464eb7185dde344d9d76a261b7e53f200f
shalinee14 <38025080+shalinee14@users.noreply.github.com>
2024-08-14 05:21:16 +0000

    learned variable and const

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,14 @@
+const accountId = 1234
+let accountName = "shalinee"
+var accountPassword = "212121"
+accountCity = "Indore"
+
+accountName = "shalinee Rathore"
+accountPassword = "010101"
+accountCity = "Alirajpur"
+
+
+/**
+ * prefer to not use var bcz of block scope
+ * / 
+console.table([accountId, accountName, accountPassword,  accountCity])
