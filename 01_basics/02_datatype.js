d5c575eec39bd85bf9cf7ba17e7250dc3dea3c06
shalinee14 <38025080+shalinee14@users.noreply.github.com>
2024-08-15 03:20:10 +0000

    learnt dataype

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,23 @@
+"use strict" // treat all js code as new version
+
+alert(3 + 3) // we are using node js not browser
+
+//ECMA provides standard for all js
+
+let name = "shalinee"
+let age = 18
+let state
+
+// number = 2 to power 53
+//biginit = this can*large number) use for big application like facebook  , reddit
+// string => ""
+// boolean => true/false
+// null => standalone value 
+// undefined => 
+// symbol => unique 
+
+
+// object 
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
